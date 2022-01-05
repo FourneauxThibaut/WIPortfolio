10b0a9b196555f26b2c55f65abe145037c476ef3
Thibaut Fourneaux <fourneaux.thibaut@gmail.com>
2022-01-05 15:28:19 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// npm button => sweetalert2
+document.getElementById('npm').onclick = function() {
+      Swal.fire({
+            background: '#434343',
+            padding: '3em',
+            title: "<h5 style='color:white'><b style='word-spacing: 0.6rem; letter-spacing: 0.05rem'>" + "npx mtkuma" + "</b></h5>",
+            html: "<i style='color: rgba(255, 255, 255, 0.7)'>" + "open a terminal and type that!" + "</i>",
+            color: '#FFFFFFF',
+            showConfirmButton: false,
+      })
+}
+
+// align portfolio_item_link to right for single items
+let portfolioLink = document.querySelectorAll('.portfolio__list__item__footer__link');
+portfolioLink.forEach(element => {
+      if (element.children.length <= 1){
+            console.log('oui');
+            element.querySelector('li').style.marginLeft = "50%"
+      }
+});
---
+++ assets/js/theme.js
@@ -0,0 +1,41 @@
+document.documentElement.classList.add('dark')
+      
+function turnThemeLight() {
+      document.getElementById('toggleTheme').classList.remove('toggleOn')
+      document.documentElement.classList.remove('dark')
+      document.getElementById('toggleTheme').classList.add('toggleOff')
+      document.documentElement.classList.add('light')
+      document.getElementById("sun").style.visibility = "visible"; 
+      document.getElementById("moon").style.visibility = "hidden"; 
+      Cookies.set('theme', 'light', { sameSite: 'strict', expires: 365, secure: true })
+}
+
+function turnThemeDark() {
+      document.getElementById('toggleTheme').classList.remove('toggleOff')
+      document.documentElement.classList.remove('light')
+      document.getElementById('toggleTheme').classList.add('toggleOn')
+      document.documentElement.classList.add('dark')
+      document.getElementById("moon").style.visibility = "visible"; 
+      document.getElementById("sun").style.visibility = "hidden"; 
+      Cookies.set('theme', 'dark', { sameSite: 'strict', expires: 365, secure: true })
+}
+
+function toggleThemeButton() {
+      let buttonClassList = document.documentElement.classList
+      let tempArrClassList = [...buttonClassList]
+
+      tempArrClassList.forEach(element => {
+            if (element == 'dark' || element == 'light'){
+                  if (element != 'dark'){ turnThemeDark() } 
+                  else{ turnThemeLight() }
+            }
+      })   
+}
+
+function setThemewithCookiesMemory() {
+      if ( (Cookies.get('theme') == undefined) ) return console.log('Cookies not set!')
+      if ( Cookies.get('theme') === 'light' ) return turnThemeLight()
+      else return turnThemeDark()
+}
+
+setThemewithCookiesMemory()
